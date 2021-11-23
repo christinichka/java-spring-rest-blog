a5f56c03c91148e593871e72f749f64a01c4fbfa
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-11-23 14:03:10 -0600

    Update PostRepository.java
    Changing to interface.

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,29 +1,15 @@
 package com.pluralsight.blog.data;
 
-import com.pluralsight.blog.model.Author;
-import com.pluralsight.blog.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Component;
+import com.pluralsight.blog.model.Post;
 
 @Component
-public class PostRepository {
-    public List<Post> findAll() {
-        return new ArrayList<>();
-    }
-
-    public List<Post> saveAll(List<Post> posts) {
-        return new ArrayList<>();
-    }
-
-    public Post save(Post post) {
-        return null;
-    }
-
-    public Optional<Post> findById(Long id) {
-        return null;
-    }
+public interface PostRepository extends JpaRepository<Post, Long> {
+	// Annotate a Repository with this to influence how it is exported and what the value of the rel attribute will be in links
+	@RestResource(rel="contains-title", path="containsTitle")
+	public List<Post> findByTitleContaining(String title);
+	public List<Post> findByAuthor_Lastname(String lastname);
 }
